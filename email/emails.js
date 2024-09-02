7c925ae1eb21eb4c16c4fe8d4a907cb3ba44b5c4
Momin Khan <mominkhan.dev@gmail.com>
2024-09-03 02:46:23 +0500

    feat: email using mailtrap

--- email/emails.js
+++ email/emails.js
@@ -1,13 +1,15 @@
 import { mailtrapClient, sender } from "./mailtrap.config.js";
-import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
+import {
+  PASSWORD_RESET_REQUEST_TEMPLATE, PASSWORD_RESET_SUCCESS_TEMPLATE,
+  VERIFICATION_EMAIL_TEMPLATE,
+} from "./emailTemplates.js";
+import {ApiError} from "../utils/ApiError.js";
 
 export const sendVerificationEmail = async (email, verificationToken) => {
-  const recipient = [{ email }];
-
   try {
     const response = await mailtrapClient.send({
       from: sender,
-      to: recipient,
+      to: [{ email }],
       subject: "Verify your email",
       html: VERIFICATION_EMAIL_TEMPLATE.replace(
         "{verificationCode}",
@@ -16,11 +18,74 @@ export const sendVerificationEmail = async (email, verificationToken) => {
       category: "Email Verification",
     });
 
+    if (!response) return null;
+
     console.log("Email sent successfully", response);
+    return response.success;
   } catch (error) {
     console.error(`Error sending verification`, error.message);
+    throw new ApiError(400, `error: ${error.message}`);
   }
 };
 
+export const sendWelcomeEmail = async (email, name) => {
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: [{ email }],
+      template_uuid: "9d023a64-d782-45de-830b-1af2f096ea17",
+      template_variables: {
+        company_info_name: "Momin Enterprises",
+        name: name,
+      },
+    });
 
-// sendVerificationEmail("mominkhan.dev@gmail.com", "Random123@");
+    if (!response) return null;
+
+    console.log("Welcome email sent successfully", response);
+    return response.success;
+  } catch (error) {
+    console.error(`Error sending welcome email`, error.message);
+    throw new ApiError(400, `error: ${error.message}`);
+  }
+};
+
+export const sendPasswordResetEmail = async (email, resetURL) => {
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: [{ email }],
+      subject: "Reset your password",
+      html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
+      category: "Password Reset",
+    });
+
+    if (!response) return null;
+
+    console.log("Email sent successfully",response);
+    return response.success;
+  } catch (error) {
+    console.error(`Error sending password reset email`, error.message);
+    throw new ApiError(400, `error: ${error.message}`);
+  }
+};
+
+export const sendResetSuccessEmail = async (email) => {
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: [{ email }],
+      subject: "Password Reset Successful",
+      html: PASSWORD_RESET_SUCCESS_TEMPLATE,
+      category: "Password Reset",
+    });
+
+    if (!response) return null;
+
+    console.log("email sent successfully", response);
+    return response.success;
+  } catch (error) {
+    console.error(`Error sending password reset success email`, error.message);
+    throw new ApiError(400, `error: ${error.message}`);
+  }
+};
